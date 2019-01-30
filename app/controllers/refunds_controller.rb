8ad358ffa5d00573e75969b71e5c772088ca84f9
morishita.hiromitsu <morishita.hiromitsu@actindi.net>
2019-01-30 16:00:04 +0900

    エラー処理の追加

--- app/controllers/refunds_controller.rb
+++ app/controllers/refunds_controller.rb
@@ -13,5 +13,12 @@ class RefundsController < ApplicationController
       charge: charge_id,
       amount: refund_amount
     )
+  rescue => e
+    puts e.class.name
+    puts e.to_s
+    puts e.message
+    puts e.backtrace
+    flash[:error] = e.to_s
+    redirect_to new_refund_path(charge_id: charge_id)
   end
 end
