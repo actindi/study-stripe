c1b698c78647f6d936a3dbaec09bb73bef2bd421
morishita-ai <morishita.hiromitsu@actindi.net>
2020-11-08 12:01:42 +0900

    apply diff and webpacker:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :charges
+  resources :refunds
+  resources :customers do
+    post :add_card
+    post :remove_card
+  end
 end
