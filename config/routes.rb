71b17b1ce476c6fb181197827befa17faca58cd6
morishita-ai <morishita.hiromitsu@actindi.net>
2020-11-08 11:40:23 +0900

    run rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #
-  resources :charges
-  resources :refunds
-  resources :customers do
-    post :add_card
-    post :remove_card
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
