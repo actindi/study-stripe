14616487292c738487040f2eee0695b8edee7998
morishita-ai <morishita.hiromitsu@actindi.net>
2020-11-08 16:19:18 +0900

    refactoring

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,26 +1,26 @@
-class ChargesController < ApplicationController
+# frozen_string_literal: true
 
+class ChargesController < ApplicationController
   def index
     @charges = Stripe::Charge.list(limit: 10)
   end
 
-  def new
-  end
+  def new; end
 
   def create
     # Amount in JPY
     @amount = 500
 
     customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source => params[:stripeToken],
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
     )
 
     charge = Stripe::Charge.create(
-      :customer => customer.id,
-      :amount => @amount,
-      :description => 'Rails Stripe ご利用料金',
-      :currency => 'jpy',
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe ご利用料金',
+      currency: 'jpy',
     )
 
   rescue Stripe::CardError => e
